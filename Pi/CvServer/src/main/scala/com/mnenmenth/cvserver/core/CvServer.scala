a3fdb5159c83e229c3b52558bdc3de8028ae29b6
Mnenmenth <iamrazim12@gmail.com>
2017-03-02 16:08:53 -0500

    Cv Server and Client update

--- Pi/CvServer/src/main/scala/com/mnenmenth/cvserver/core/CvServer.scala
+++ Pi/CvServer/src/main/scala/com/mnenmenth/cvserver/core/CvServer.scala
@@ -2,17 +2,16 @@ package com.mnenmenth.cvserver.core
 
 import java.awt.{Dimension, Graphics}
 import java.awt.image.BufferedImage
-import java.io.{BufferedReader, ByteArrayInputStream, InputStreamReader}
-import java.net.Socket
+import java.io._
+import java.net.{InetAddress, InetSocketAddress, ServerSocket, Socket}
 import java.util.concurrent.{ExecutorService, Executors}
 import javax.imageio.ImageIO
 import javax.swing.{JFrame, JLabel, JPanel, SwingUtilities}
 
 import com.mnenmenth.cvserver.util.Util
-import org.opencv.core.{Mat, MatOfByte}
+import org.opencv.core.{Core, CvType, Mat, MatOfByte}
 import org.opencv.videoio.VideoCapture
 import org.opencv.imgcodecs.Imgcodecs
-import org.opencv.core.CvType
 import org.opencv.videoio.Videoio
 
 import scala.collection.mutable
@@ -30,16 +29,39 @@ object CvServer {
 
   def main(args: Array[String]): Unit = {
 
+    System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
+
     val cap = new VideoCapture()
     cap.open("/dev/stdin")
 
     val rt = Runtime.getRuntime
     val proc = rt.exec("COMMAND")
     val isr = new InputStreamReader(proc.getInputStream)
+
     val br = new BufferedReader(isr)
 
     val mat = Mat.zeros(CvServer.vidsize.width, CvServer.vidsize.height, CvType.CV_8UC3)
-    cap.retrieve(mat)
+
+    while(!cap.isOpened) Thread.sleep(10)
+
+    while(mat.empty()) cap.retrieve(mat)
+
+    val socket = new ServerSocket(2223)
+    while(true){
+      val client = socket.accept()
+      while(client.isConnected) {
+        cap.retrieve(mat)
+        pool.submit(new Runnable {
+          override def run(): Unit = {
+            val writer = new DataOutputStream(client.getOutputStream)
+            val buff = new Array[Byte]((mat.total()*mat.elemSize()).toInt)
+            mat.get(0, 0, buff)
+            writer.write(buff)
+          }
+        })
+      }
+    }
+    pool.shutdownNow()
 
   }
 
